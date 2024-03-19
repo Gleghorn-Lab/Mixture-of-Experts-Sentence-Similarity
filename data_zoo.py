4436dd380d1b4073fb670936cb21914dd02be8b3
lhallee <lhallee99@gmail.com>
2024-03-19 11:56:33 -0400

    more bug fix

--- data_zoo.py
+++ data_zoo.py
@@ -9,7 +9,7 @@ def data_collator(features):
 
 
 class TextDataset(TorchDataset):
-    def __init__(self, a, b, c_labels, r_labels, tokenizer, domains, max_length=512):
+    def __init__(self, a, b, c_labels, r_labels, tokenizer, domains, add_tokens, max_length=512):
         self.a = a
         self.b = b
         self.c_labels = c_labels
@@ -17,6 +17,7 @@ class TextDataset(TorchDataset):
         self.tokenizer = tokenizer
         self.domains = domains
         self.max_length = max_length
+        self.add_tokens = add_tokens
 
     def __len__(self):
         return len(self.a)
@@ -24,7 +25,6 @@ class TextDataset(TorchDataset):
     def __getitem__(self, idx): # Maybe need a version for non MOE
         r_label = torch.tensor(self.r_labels[idx], dtype=torch.long)
         c_label = torch.tensor(self.c_labels[idx], dtype=torch.float)
-        domain_token = self.tokenizer(self.domains[int(r_label.item())], add_special_tokens=False).input_ids[0]  # get the domain token
         tokenized_a = self.tokenizer(self.a[idx],
                                      return_tensors='pt',
                                      padding='max_length',
@@ -35,8 +35,11 @@ class TextDataset(TorchDataset):
                                      padding='max_length',
                                      truncation=True,
                                      max_length=self.max_length)
-        tokenized_a['input_ids'][0][0] = domain_token  # replace the cls token with the domain token
-        tokenized_b['input_ids'][0][0] = domain_token  # replace the cls token with the domain token
+        if self.add_tokens:
+            domain_token = self.tokenizer(self.domains[int(r_label.item())],
+                                          add_special_tokens=False).input_ids[0]  # get the domain token
+            tokenized_a['input_ids'][0][0] = domain_token  # replace the cls token with the domain token
+            tokenized_b['input_ids'][0][0] = domain_token  # replace the cls token with the domain token
         return {
             'input_ids_a': tokenized_a['input_ids'].squeeze(),
             'attention_mask_a': tokenized_a['attention_mask'].squeeze(),
@@ -50,10 +53,12 @@ class TextDataset(TorchDataset):
 def get_datasets_train(args, tokenizer):
     data_paths = args['data_paths']
     domains = args['domains']
+    add_tokens = args['new_special_tokens']
     max_length = args['max_length']
     a_col = args['a_col']
     b_col = args['b_col']
     label_col = args['label_col']
+
     train_a, train_b, train_c_label, train_r_label = [], [], [], []
     valid_a, valid_b, valid_c_label, valid_r_label = [], [], [], []
     test_a, test_b, test_c_label, test_r_label = [], [], [], []
@@ -75,17 +80,18 @@ def get_datasets_train(args, tokenizer):
         test_c_label.extend(test[label_col])
         test_r_label.extend([i] * len(test[label_col]))
     train_dataset = TextDataset(train_a, train_b, train_c_label, train_r_label,
-                                tokenizer, domains, max_length)
+                                tokenizer, domains, add_tokens, max_length)
     valid_dataset = TextDataset(valid_a, valid_b, valid_c_label, valid_r_label,
-                                tokenizer, domains, max_length)
+                                tokenizer, domains, add_tokens,  max_length)
     test_dataset = TextDataset(test_a, test_b, test_c_label, test_r_label,
-                               tokenizer, domains, max_length)
+                               tokenizer, domains, add_tokens,  max_length)
     return train_dataset, valid_dataset, test_dataset
 
 
 def get_datasets_test(args, tokenizer):
     data_paths = args['data_paths']
     domains = args['domains']
+    add_tokens = args['new_special_tokens']
     max_length = args['max_length']
     a_col = args['a_col']
     b_col = args['b_col']
@@ -108,7 +114,7 @@ def get_datasets_test(args, tokenizer):
         test_c_label.extend(test[label_col])
         test_r_label.extend([i] * len(test[label_col]))
         valid_datasets.append(TextDataset(valid_a, valid_b, valid_c_label, valid_r_label,
-                                          tokenizer, domains, max_length))
+                                          tokenizer, domains, add_tokens, max_length))
         test_datasets.append(TextDataset(test_a, test_b, test_c_label, test_r_label,
-                                         tokenizer, domains, max_length))
+                                         tokenizer, domains, add_tokens, max_length))
     return valid_datasets, test_datasets

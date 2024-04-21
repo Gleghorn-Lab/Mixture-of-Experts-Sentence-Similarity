8185e2ffd861a7194dc7d12dbfe54990839f889c
Logan Hallee <lhallee@udel.edu>
2024-04-21 14:20:19 -0400

    yaml and trainer fix

--- trainer.py
+++ trainer.py
@@ -5,6 +5,16 @@ from torch.nn import functional as F
 from typing import Dict, Union, Any
 from transformers import Trainer, TrainingArguments, EarlyStoppingCallback
 from transformers.trainer_callback import TrainerCallback, TrainerControl, TrainerState
+### For custom trainer
+import numpy as np
+from typing import Optional, List
+from torch.utils.data import DataLoader
+from transformers.integrations.deepspeed import deepspeed_init
+from transformers.trainer_utils import EvalLoopOutput, EvalPrediction, has_length, denumpify_detensorize
+from transformers.trainer_pt_utils import EvalLoopContainer, IterableDatasetShard, find_batch_size
+from transformers.utils import logging
+
+logger = logging.get_logger(__name__)
 
 
 class TopkTallyCallback(TrainerCallback):
@@ -28,6 +38,8 @@ class TopkTallyCallback(TrainerCallback):
 
 class DoubleTrainer(Trainer):
     def __init__(self, *args, **kwargs):
+
+
         self.loss = kwargs.pop('loss', None)
         self.temp = torch.tensor(kwargs.pop("temp", 1.0))
         super().__init__(*args, **kwargs)
@@ -50,9 +62,6 @@ class DoubleTrainer(Trainer):
                 emb_a = torch.cat(self.accumulated_a, dim=0)
                 emb_b = torch.cat(self.accumulated_b, dim=0)
                 loss = self.loss(emb_a, emb_b, self.temp)
-
-                print('Loss calculated: ', loss)
-
                 if self.args.n_gpu > 1:
                     loss = loss.mean()  # mean() to average on multi-gpu parallel training
 
@@ -71,100 +80,73 @@ class DoubleTrainer(Trainer):
         ignore_keys: Optional[List[str]] = None,
         metric_key_prefix: str = "eval",
     ) -> EvalLoopOutput:
-        """
-        Prediction/evaluation loop, shared by `Trainer.evaluate()` and `Trainer.predict()`.
-
-        Works both with or without labels.
-        """
+        ### Reset accumulated logits
+        self.accumulated_a = []
+        self.accumulated_b = []
         args = self.args
+        gradient_accumulation_steps = args.gradient_accumulation_steps
 
         prediction_loss_only = prediction_loss_only if prediction_loss_only is not None else args.prediction_loss_only
-
-        # if eval is called w/o train, handle model prep here
         if self.is_deepspeed_enabled and self.deepspeed is None:
             _, _ = deepspeed_init(self, num_training_steps=0, inference=True)
-
         model = self._wrap_model(self.model, training=False, dataloader=dataloader)
-
         if len(self.accelerator._models) == 0 and model is self.model:
             model = (
                 self.accelerator.prepare(model)
                 if self.is_deepspeed_enabled
                 else self.accelerator.prepare_model(model, evaluation_mode=True)
             )
-
             if self.is_fsdp_enabled:
                 self.model = model
-
-            # for the rest of this function `model` is the outside model, whether it was wrapped or not
             if model is not self.model:
                 self.model_wrapped = model
-
-            # backward compatibility
             if self.is_deepspeed_enabled:
                 self.deepspeed = self.model_wrapped
-
-        # if full fp16 or bf16 eval is wanted and this ``evaluation`` or ``predict`` isn't called
-        # while ``train`` is running, cast it to the right dtype first and then put on device
         if not self.is_in_train:
             if args.fp16_full_eval:
                 model = model.to(dtype=torch.float16, device=args.device)
             elif args.bf16_full_eval:
                 model = model.to(dtype=torch.bfloat16, device=args.device)
-
         batch_size = self.args.eval_batch_size
-
         logger.info(f"***** Running {description} *****")
         if has_length(dataloader):
             logger.info(f"  Num examples = {self.num_examples(dataloader)}")
         else:
             logger.info("  Num examples: Unknown")
         logger.info(f"  Batch size = {batch_size}")
-
         model.eval()
-
         self.callback_handler.eval_dataloader = dataloader
-        # Do this before wrapping.
         eval_dataset = getattr(dataloader, "dataset", None)
-
         if args.past_index >= 0:
             self._past = None
-
-        # Initialize containers
         all_losses = EvalLoopContainer(self.args.eval_do_concat_batches, padding_index=-100)
         all_preds = EvalLoopContainer(self.args.eval_do_concat_batches, padding_index=-100)
         all_labels = EvalLoopContainer(self.args.eval_do_concat_batches, padding_index=-100)
         all_inputs = EvalLoopContainer(self.args.eval_do_concat_batches, padding_index=-100)
-
-        # Will be useful when we have an iterable dataset so don't know its length.
         observed_num_examples = 0
-
-        # Main evaluation loop
         for step, inputs in enumerate(dataloader):
-            # Update the observed num examples
             observed_batch_size = find_batch_size(inputs)
             if observed_batch_size is not None:
                 observed_num_examples += observed_batch_size
-                # For batch samplers, batch_size is not known by the dataloader in advance.
                 if batch_size is None:
                     batch_size = observed_batch_size
-
-            # Prediction step
+            ### Primarily updated here
             loss, logits, labels = self.prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
+            a, b = logits[0], logits[1]
             main_input_name = getattr(self.model, "main_input_name", "input_ids")
             inputs_decode = self._prepare_input(inputs[main_input_name]) if args.include_inputs_for_metrics else None
-
-            if is_torch_xla_available():
-                xm.mark_step()
-
-            """
-            TODO TODO TODO TODO
-            """
-
-            # Update containers
-            if loss is not None: ### based on step and grad accum, calculate and handle the losses correctly
-                losses = self.gather_function((loss.repeat(batch_size)))
+            self.accumulated_a.append(a)
+            self.accumulated_b.append(b)
+            if len(self.accumulated_a) == self.args.gradient_accumulation_steps:
+                emb_a = torch.cat(self.accumulated_a, dim=0)
+                emb_b = torch.cat(self.accumulated_b, dim=0)
+                loss = self.loss(emb_a, emb_b, self.temp)
+                print('Eval loss updated: ', loss)
+                losses = self.gather_function((loss.repeat(batch_size * gradient_accumulation_steps)))
                 all_losses.add(losses)
+                self.accumulated_a = []
+                self.accumulated_b = []
+            ###
             if inputs_decode is not None:
                 inputs_decode = self.accelerator.pad_across_processes(inputs_decode, dim=1, pad_index=-100)
                 inputs_decode = self.gather_function((inputs_decode))
@@ -182,17 +164,14 @@ class DoubleTrainer(Trainer):
 
             self.control = self.callback_handler.on_prediction_step(args, self.state, self.control)
 
-            # Gather all tensors and put them back on the CPU if we have done enough accumulation steps.
             if args.eval_accumulation_steps is not None and (step + 1) % args.eval_accumulation_steps == 0:
                 all_losses.to_cpu_and_numpy()
                 all_preds.to_cpu_and_numpy()
                 all_labels.to_cpu_and_numpy()
                 all_inputs.to_cpu_and_numpy()
 
-        # After all calls to `.gather_function`, reset to `gather_for_metrics`:
         self.gather_function = self.accelerator.gather_for_metrics
         if args.past_index and hasattr(self, "_past"):
-            # Clean the state at the end of the evaluation loop
             delattr(self, "_past")
 
         # Gather all remaining tensors and put them back on the CPU
@@ -201,11 +180,8 @@ class DoubleTrainer(Trainer):
         all_labels = all_labels.get_arrays()
         all_inputs = all_inputs.get_arrays()
 
-        # Number of samples
         if has_length(eval_dataset):
             num_samples = len(eval_dataset)
-        # The instance check is weird and does not actually check for the type, but whether the dataset has the right
-        # methods. Therefore we need to make sure it also has the attribute.
         elif isinstance(eval_dataset, IterableDatasetShard) and getattr(eval_dataset, "num_examples", 0) > 0:
             num_samples = eval_dataset.num_examples
         else:
@@ -215,7 +191,6 @@ class DoubleTrainer(Trainer):
                 num_samples = observed_num_examples
         if num_samples == 0 and observed_num_examples > 0:
             num_samples = observed_num_examples
-
         # Metrics!
         if self.compute_metrics is not None and all_preds is not None and all_labels is not None:
             if args.include_inputs_for_metrics:
@@ -226,8 +201,6 @@ class DoubleTrainer(Trainer):
                 metrics = self.compute_metrics(EvalPrediction(predictions=all_preds, label_ids=all_labels))
         else:
             metrics = {}
-
-        # To be JSON-serializable, we need to remove numpy types or zero-d tensors
         metrics = denumpify_detensorize(metrics)
 
         if isinstance(all_losses, list) and all_losses:
@@ -236,12 +209,9 @@ class DoubleTrainer(Trainer):
             metrics[f"{metric_key_prefix}_loss"] = all_losses.mean().item()
         if hasattr(self, "jit_compilation_time"):
             metrics[f"{metric_key_prefix}_jit_compilation_time"] = self.jit_compilation_time
-
-        # Prefix all keys with metric_key_prefix + '_'
         for key in list(metrics.keys()):
             if not key.startswith(f"{metric_key_prefix}_"):
                 metrics[f"{metric_key_prefix}_{key}"] = metrics.pop(key)
-
         return EvalLoopOutput(predictions=all_preds, label_ids=all_labels, metrics=metrics, num_samples=num_samples)
 
 

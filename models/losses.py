b669b7ed3307848fe29d46c3114ae6d3d4e1384e
Logan Hallee <lhallee@udel.edu>
2025-02-14 21:17:05 -0500

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -6,8 +6,7 @@ import torch.nn.functional as F
 def mnr_loss(
     batch1: torch.Tensor,
     batch2: torch.Tensor,
-    normalize: bool = True,
-    weights: torch.Tensor = None
+    normalize: bool = False,
 ) -> torch.Tensor:
     """
     Multiple negatives ranking loss:
@@ -20,20 +19,14 @@ def mnr_loss(
 
     logits = batch1 @ batch2.T
     targets = torch.arange(len(batch1), device=batch1.device)
-    loss = F.cross_entropy(logits, targets, reduction='none')
-    if weights is not None:
-        if weights.ndim != 1 or weights.shape[0] != batch1.shape[0]:
-            raise ValueError("weights must be a 1D tensor with length equal to the batch size")
-        loss = loss * weights
-    loss = loss.mean()
+    loss = F.cross_entropy(logits, targets)
     return loss
 
 
 def mnr_plus_loss(
     batch1: torch.Tensor,
     batch2: torch.Tensor,
-    normalize: bool = True,
-    weights: torch.Tensor = None
+    normalize: bool = False,
 ) -> float:
     """
     Computes a variant of the multiple negatives ranking loss by computing
@@ -46,13 +39,8 @@ def mnr_plus_loss(
     logits = batch1 @ batch2.T
     batch1_similarity = batch1 @ batch1.T
     batch2_similarity = batch2 @ batch2.T
-    targets = (batch1_similarity + batch2_similarity) / 2.0
-    loss1 = F.cross_entropy(logits, targets.argmax(dim=1), reduction='none')
-    loss2 = F.cross_entropy(logits.T, targets.T.argmax(dim=1), reduction='none')
-    loss = loss1 + loss2
-    if weights is not None:
-        if weights.ndim != 1 or weights.shape[0] != batch1.shape[0]:
-            raise ValueError("weights must be a 1D tensor with length equal to the batch size")
-        loss = loss * weights
-    loss = loss.mean()
+    targets = F.softmax((batch1_similarity + batch2_similarity) / 2.0, dim=-1)
+    loss1 = F.cross_entropy(logits, targets.argmax(dim=1))
+    loss2 = F.cross_entropy(logits.T, targets.T.argmax(dim=1))
+    loss = (loss1 + loss2) / 2.0
     return loss
